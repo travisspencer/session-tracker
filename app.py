a0330106f62a95f31e5e6ee5784e070f4090139d
Travis Spencer <travis@curity.io>
2019-02-22 16:27:08 +0100

    track user agent ID

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-import os
+import random
 import ssl
+import string
 import time
 import urllib
 import urllib2
 from datetime import datetime, timedelta
 
-from flask import Flask, render_template, request, json, session as flask_sesion, abort
+from flask import Flask, render_template, request, json, session as flask_sesion, abort, make_response
 
 app = Flask(__name__)
 
@@ -53,7 +54,7 @@ def get_scim_access_token():
     return scim_access_token
 
 
-def get_all_sessions_for_user(username):
+def get_all_delegations_for_user(username, retry=True):
     resources = delegations = []
 
     if username:
@@ -61,19 +62,27 @@ def get_all_sessions_for_user(username):
     else:
         return delegations
 
-    data = {
-        "attributes": "externalId,id,sub,exp,clientId",
-        # This should work but don't due to an apparent bug in Curity
-        # "filter": 'userName eq "%s"' % username
-    }
     scim_access_token = get_scim_access_token()
 
     if scim_access_token:
-        scim_response = urllib2.urlopen(
-            urllib2.Request("%s%s?%s" % (oauth_server, scim_delegation_uri, urllib.urlencode(data)),
-                            headers=dict(Authorization="bearer %s" % scim_access_token)),
-            context=ssl_context())
-        scim_response_json = json.loads(scim_response.read())
+        scim_response_json = {}
+        data = {
+            "attributes": "externalId,id,sub,exp,clientId",
+            # This should work but don't due to an apparent bug in Curity
+            # "filter": 'userName eq "%s"' % username
+        }
+
+        try:
+            scim_response = urllib2.urlopen(
+                urllib2.Request("%s%s?%s" % (oauth_server, scim_delegation_uri, urllib.urlencode(data)),
+                                headers=dict(Authorization="bearer %s" % scim_access_token)),
+                context=ssl_context())
+            scim_response_json = json.loads(scim_response.read())
+        except urllib2.HTTPError as e:
+            app.logger.debug("SCIM call failed; token may be expired; retrying. %s", e)
+
+            if retry:
+                get_all_delegations_for_user(username, False)
 
         resources = scim_response_json.get("Resources", [])
 
@@ -100,7 +109,7 @@ def delete():
     if "all" in request.args:
         # Get all the sessions from the SCIM server right now in case new sessions have been initiated at some other
         # client. The ones in the current session may not be all of them.
-        for delegation in get_all_sessions_for_user(flask_sesion.get("username", [])):
+        for delegation in get_all_delegations_for_user(flask_sesion.get("username", [])):
             delete_delegation(delegation["id"])
 
         return logout()
@@ -110,13 +119,19 @@ def delete():
         delegations = flask_sesion["delegations"] = [d for d in flask_sesion["delegations"] if
                                                      d.get("id", None) != delegation_id]
 
-    return render_template("index.html",
-                           # Which client ID is added by JavaScript, client-side upon selection
-                           authorization_uri="%s%s?response_type=code&scope=%s&redirect_uri=%scb&client_id=" % (
-                               oauth_server, authorize_uri, scope, request.host_url),
-                           delegations=delegations,
-                           username=flask_sesion["username"],
-                           logout_uri="%s%s?redirect_uri=%slogout" % (oauth_server, logout_uri, request.host_url))
+    user_agent_id = get_user_agent_id()
+    response = make_response(render_template("index.html",
+                                             user_agent_id=user_agent_id,
+                                             # Which client ID is added by JavaScript, client-side upon selection
+                                             authorization_uri="%s%s?response_type=code&scope=%s&redirect_uri=%scb&client_id=" % (
+                                                 oauth_server, authorize_uri, scope, request.host_url),
+                                             delegations=delegations,
+                                             username=flask_sesion["username"],
+                                             logout_uri="%s%s?redirect_uri=%slogout" % (
+                                                 oauth_server, logout_uri, request.host_url)))
+    response.set_cookie("user_agent_id", user_agent_id)
+
+    return response
 
 
 def delete_delegation(delegation_id, retry=True):
@@ -165,7 +180,7 @@ def introspect():
         else:
             return "Introspection was not possible"
     except urllib2.HTTPError as e:
-        app.logger.debug("Introspection failed", e)
+        app.logger.debug("Introspection failed %s", e)
 
         return "Failed to introspect token due to an error"
 
@@ -202,9 +217,11 @@ def callback():
     if code:
         client_id_in_use = request.args.get("state", client_id)
 
+        user_agent_id = get_user_agent_id()
         data = {'client_id': client_id_in_use, "client_secret": client_secret,
                 'code': code,
                 'redirect_uri': request.base_url,
+                'user_agent_id': user_agent_id,
                 'grant_type': 'authorization_code'}
         try:
             token_response = urllib2.urlopen(
@@ -212,9 +229,13 @@ def callback():
                 context=ssl_context())
             token_response_json = json.loads(token_response.read())
 
+            # Make sure the response includes the same user agent ID that we send
+            if token_response_json.get("user_agent_id", None) != user_agent_id:
+                abort(500)
+
             # Save all delegations returned from the SCIM server in the user's session
             flask_sesion["username"] = username = token_response_json.get("sub", None)
-            delegations = get_all_sessions_for_user(username)
+            delegations = get_all_delegations_for_user(username)
 
             delegation_id = token_response_json.get("delegation_id", None)
 
@@ -227,17 +248,23 @@ def callback():
             flask_sesion["delegations"] = delegations
         except urllib2.HTTPError as e:
             if e.code == 400:
-                # User probably refreshed the page.
+                # User probably (or at least hopefully) refreshed the page.
+                app.logger.debug("Could not complete callback handling due to an invalid request: %s", e)
+
                 delegations = flask_sesion.get("delegations", [])
             else:
                 raise e
 
-    return render_template("index.html",
-                           authorization_uri="%s%s?response_type=code&scope=%s&redirect_uri=%scb&client_id=" % (
-                               oauth_server, authorize_uri, scope, request.host_url),
-                           delegations=delegations,
-                           username=flask_sesion.get("username", ""),
-                           logout_uri="%s%s?redirect_uri=%slogout" % (oauth_server, logout_uri, request.host_url))
+    response = make_response(render_template("index.html", user_agent_id=user_agent_id,
+                                             authorization_uri="%s%s?response_type=code&scope=%s&redirect_uri=%scb&client_id=" % (
+                                                 oauth_server, authorize_uri, scope, request.host_url),
+                                             delegations=delegations,
+                                             username=flask_sesion.get("username", ""),
+                                             logout_uri="%s%s?redirect_uri=%slogout" % (
+                                                 oauth_server, logout_uri, request.host_url)))
+    response.set_cookie("user_agent_id", user_agent_id)
+
+    return response
 
 
 def ssl_context():
@@ -255,14 +282,29 @@ def logout():
     return index()
 
 
+def get_user_agent_id():
+    cookie_value = request.cookies.get('user_agent_id', generate_random_string(5))
+
+    return cookie_value
+
+
+def generate_random_string(size=20):
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(size))
+
+
 @app.route('/')
 def index():
-    return render_template("index.html",
-                           authorization_uri="%s%s?response_type=code&scope=%s&redirect_uri=%scb&client_id=" % (
-                               oauth_server, authorize_uri, scope, request.host_url))
+    user_agent_id = get_user_agent_id()
+    response = make_response(render_template("index.html",
+                                             user_agent_id=user_agent_id,
+                                             authorization_uri="%s%s?response_type=code&scope=%s&redirect_uri=%scb&client_id=" % (
+                                                 oauth_server, authorize_uri, scope, request.host_url)))
+    response.set_cookie("user_agent_id", user_agent_id)
+
+    return response
 
 
 if __name__ == '__main__':
-    app.secret_key = os.urandom(16)
+    app.secret_key = generate_random_string()
     app.jinja_env.filters['datetime'] = lambda value: time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(value))
     app.run(debug=True)
